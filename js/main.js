4e6ad840fb72967448b446fce351852cd43394ec
Alicia Gallego Gonzalez <aliciagallegonz@gmail.com>
2022-02-21 18:08:48 +0100

    function game + style

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
-'use strict'
+'use strict';
 
-function getRandomInt() {
-    return Math.floor(Math.random() * 6);
+const cube = document.querySelector('.cube');
+
+function game() {
+  cube.style.transform = 'rotateX(0deg) rotateZ(0deg) rotateY(0deg)';
+  const randomNumber = Math.floor(Math.random() * 5 + 1);
+  console.log(randomNumber);
+  if (randomNumber === 1) {
+    cube.style.transform = 'rotateX(3600deg) rotateZ(3600deg) rotateY(3600deg)';
+  } else if (randomNumber === 2) {
+    cube.style.transform = 'rotateX(800deg) rotateZ(3600deg) rotateY(3600deg)';
+  } else if (randomNumber === 3) {
+    cube.style.transform = 'rotateX(3600deg) rotateZ(3600deg) rotateY(2430deg)';
+  } else if (randomNumber === 4) {
+    cube.style.transform = 'rotateX(3600deg) rotateZ(3600deg) rotateY(4410deg)';
+  } else if (randomNumber === 5) {
+    cube.style.transform = 'rotateX(2430deg) rotateZ(3600deg) rotateY(3600deg)';
+  } else if (randomNumber === 6) {
+    cube.style.transform = 'rotateX(3600deg) rotateZ(3600deg) rotateY(1980deg)';
   }
-  
-  console.log(getRandomInt());
-
-
-function game(){
-
 }
+
+cube.addEventListener('click', game);
